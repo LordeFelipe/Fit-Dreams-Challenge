a00d2f4abd63941256e18da8b8a0923dce701ec0
Felipe <felipevaz444@gmail.com>
2022-06-02 13:09:58 -0300

    :white_check_mark: Model tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,48 @@ RSpec.describe User, type: :model do
       it { expect(build(:admin)).to be_valid }
     end
   end
+
+  describe 'validations' do
+    context 'when it doesnt have a name' do
+      it { expect(build(:student, name: '')).to be_invalid }
+    end
+
+    context 'when it doesnt have a birthdate' do
+      it { expect(build(:student, birthdate: '')).to be_invalid }
+    end
+
+    context 'when it doesnt have a email' do
+      it { expect(build(:student, email: '')).to be_invalid }
+    end
+
+    context 'when it doesnt have a role' do
+      it { expect(build(:student, role_id: '')).to be_invalid }
+    end
+
+    context 'when the role does not exist' do
+      it { expect(build(:student, role_id: -1)).to be_invalid }
+    end
+
+    context 'when the password does not exist' do
+      it { expect(build(:student, password: '')).to be_invalid }
+    end
+
+    context 'when the password is to short' do
+      it { expect(build(:student, password: '123')).to be_invalid }
+    end
+
+    context 'when the email is not unique' do
+      it do
+        create(:student, email: 'email@email.com')
+        expect(build(:student, email: 'email@email.com')).to be_invalid
+      end
+    end
+
+    context 'when the email is unique' do
+      it do
+        create(:student, email: 'email@email.com')
+        expect(build(:student, email: 'felipe@vaz.com')).to be_valid
+      end
+    end
+  end
 end
